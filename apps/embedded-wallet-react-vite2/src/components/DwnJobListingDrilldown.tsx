588475a2218c8958c59bd7a0f3c6c1e1073fa541
reisepass <robert@bla.com>
2023-11-29 12:27:55 -0500

    fixed and added protocols

--- apps/embedded-wallet-react-vite2/src/components/DwnJobListingDrilldown.tsx
+++ apps/embedded-wallet-react-vite2/src/components/DwnJobListingDrilldown.tsx
@@ -11,31 +11,118 @@ export const DwnJobListingDrilldown: FC = () => {
     const runWeb5 = async () => {
       const { web5, did: myDid } = await Web5.connect({ sync: "5s" });
 
-      const applicationProtocolWithoutDirectJobLink = JSON.parse(`{
-          "protocol": "https://didcomm.org/uris/that/dont/resolve/are/funny/selfProfileProtocol",
-    "published": true,
-    "types": {
-      "selfprofile": {
-        "schema": "https://didcomm.org/uris/that/dont/resolve/are/funny/selfprofile.json",
-        "dataFormats": ["application/json"]
-      }
-    },
-    "structure": {
-      "selfprofile": {
-        "$actions": [
-          {
-            "who": "author",
-            "of": "selfprofile",
-            "can": "write"
-          },
-          {
-            "who": "anyone",
-            "can": "read"
+
+
+     let applicationProtocolWithoutDirectJobLink = JSON.parse(`{
+      "protocol": "https://didcomm.org/uris/that/dont/resolve/are/funny/applicationProtocolWithoutDirectJobLink",
+        "published": true,
+        "types": {
+          "japplication": {
+            "schema": "https://didcomm.org/uris/that/dont/resolve/are/funny/japplication.json",
+            "dataFormats": ["application/json"]
           }
-        ]
+        },
+        "structure": {
+          "japplication": {
+            "$actions": [
+              {
+                "who": "author",
+                "of": "japplication",
+                "can": "read"
+              },
+              {
+                "who": "recipient",
+                "of": "japplication",
+                "can": "read"
+              },
+              {
+                "who": "anyone",
+                "can": "write"
+              }
+            ]
+          }
+        }
       }
-    }
-     `);
+      `);
+
+
+      let selfProfileProtocol = JSON.parse(`{
+        "protocol": "https://didcomm.org/uris/that/dont/resolve/are/funny/selfProfileProtocol",
+          "published": true,
+          "types": {
+            "selfprofile": {
+              "schema": "https://didcomm.org/uris/that/dont/resolve/are/funny/selfprofile.json",
+              "dataFormats": ["application/json"]
+            }
+          },
+          "structure": {
+            "selfprofile": {
+              "$actions": [
+                {
+                  "who": "author",
+                  "of": "selfprofile",
+                  "can": "write"
+                },
+                {
+                  "who": "anyone",
+                  "can": "read"
+                }
+              ]
+            }
+          }
+        }
+        `);
+
+
+
+        let jobPostThatCanTakeApplicationsAsReplyProtocol = JSON.parse(`{
+          "protocol": "https://didcomm.org/uris/that/dont/resolve/are/funny/jobPostProtocol",
+            "published": true,
+            "types": {
+              "jobPost": {
+                "schema": "https://didcomm.org/uris/that/dont/resolve/are/funny/jobPost.json",
+                "dataFormats": ["application/json"],
+              },
+              "japplication": {
+                "schema": "https://didcomm.org/uris/that/dont/resolve/are/funny/japplication.json",
+                "dataFormats": ["application/json"]
+            },
+            "structure": {
+              "jobPost": {
+                "$actions": [
+                  {
+                    "who": "author",
+                    "of": "jobPost",
+                    "can": "write"
+                  },
+                  {
+                    "who": "anyone",
+                    "can": "read"
+                  }
+                ],
+                "japplication": {
+                  "$actions": [
+                    {
+                      "who": "author",
+                      "of": "japplication",
+                      "can": "read"
+                    },
+                    {
+                      "who": "recipient",
+                      "of": "japplication",
+                      "can": "read"
+                    },
+                    {
+                      "who": "anyone",
+                      "can": "write"
+                    }
+                  ]
+                }
+              }
+            }
+          }
+          `);
+
 
       await web5.dwn.protocols.query({
         message: {
