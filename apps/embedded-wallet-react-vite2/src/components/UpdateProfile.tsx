d74f0e834e4213037bf25125ab239cb549061a87
A A <20057155+ad0ll@users.noreply.github.com>
2023-12-01 20:03:59 -0500

    Merge UpdateProfile changes

--- apps/embedded-wallet-react-vite2/src/components/UpdateProfile.tsx
+++ apps/embedded-wallet-react-vite2/src/components/UpdateProfile.tsx
@@ -3,25 +3,38 @@ import { Input } from "@/components/ui/input";
 import React, { useEffect, useState } from "react";
 import {
   dwnReadSelfReturnRecordAndData,
-  selfProfileProtocol,
+  dwnReadSelfReturnRecordAndDataSelector,
 } from "./lib/utils";
+import { useToast } from "@/components/ui/use-toast.ts";
+import { SubmitHandler, useForm } from "react-hook-form";
+import type { Record } from "@web5/api";
+import { useRecoilValue } from "recoil";
 
+type UpdateForm = {
+  name: string;
+};
 const UpdateProfile: React.FC = () => {
-  const [myRecord, setmyRecord] = useState<any>();
-  const [data, setdata] = useState<any>();
-  const [newName, setNewName] = useState("");
+  const [myRecord, setMyRecord] = useState<Record>();
+  const [data, setData] = useState<any>();
+  const { toast } = useToast();
+
+  const res = useRecoilValue(dwnReadSelfReturnRecordAndDataSelector);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }, //TODO Add this to the form
+  } = useForm<UpdateForm>();
 
   useEffect(() => {
     const fetchData = async () => {
-      const rec = await dwnReadSelfReturnRecordAndData(selfProfileProtocol);
+      const rec = await dwnReadSelfReturnRecordAndData();
       if (rec) {
-        setmyRecord(rec.record);
-        setdata(rec.data);
+        setMyRecord(rec.record);
+        setData(rec.data);
       }
-      console.log(
-        "🚀 ~ file: UpdateProfile.tsx:16 ~ fetchData ~ myRecord:",
-        rec,
-      );
+
+      console.debug("rec", rec);
+      console.debug("res", res);
     };
 
     fetchData();
@@ -31,34 +44,43 @@ const UpdateProfile: React.FC = () => {
     );
   }, []);
 
-  const updateName = async () => {
-    data.name = newName;
-    const { status } = await myRecord.update({ data });
+  const updateFormSubmit: SubmitHandler<UpdateForm> = async (formData) => {
+    if (!myRecord) {
+      toast({
+        title: "Error",
+        description: `Couldn't find your dwn record`,
+      });
+      return;
+    }
+    console.log("myRecord", myRecord);
+    const recc = res?.record;
+    if (!recc) return;
+    const { status } = await recc.update({
+      data: { ...res?.data, name: formData.name },
+    });
+    toast({
+      title: "Posted profile update to DWN",
+      description: `Status: ${status.code} (${status.detail})`,
+    });
     console.log(
       "🚀 ~ file: UpdateProfile.tsx:23 ~ updateName ~ status:",
       status,
     );
   };
+  if (!res) return <></>;
 
   return (
     <>
-      {" "}
       {data === undefined ? (
         <h2>Loading... </h2>
       ) : (
-        <>
+        <form>
           <h2>Hello {data.name}</h2>
-          <Input
-            name="nameInput"
-            defaultValue={data.name}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-        </>
-      )}
-      {newName ? (
-        <Button onClick={() => updateName()}>Change Name</Button>
-      ) : (
-        <> </>
+          <Input defaultValue={data.name} {...register("name")} />
+          <Button type={"submit"} onClick={handleSubmit(updateFormSubmit)}>
+            Update
+          </Button>
+        </form>
       )}
     </>
   );
