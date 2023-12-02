f6afd151b4f022db39e6c83cf3d3c1b49e0384f2
A A <20057155+ad0ll@users.noreply.github.com>
2023-12-01 19:49:57 -0500

    I broke it

--- apps/embedded-wallet-react-vite2/src/components/UpdateProfile.tsx
+++ apps/embedded-wallet-react-vite2/src/components/UpdateProfile.tsx
@@ -1,39 +1,45 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React, { useEffect, useState } from "react";
-import {
-  dwnReadSelfReturnRecordAndData,
-  selfProfileProtocol,
-} from "./lib/utils";
+import React from "react";
+import { dwnReadSelfReturnRecordAndDataSelector } from "./lib/utils";
+import { useToast } from "@/components/ui/use-toast.ts";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useRecoilValue } from "recoil";
 
+type UpdateForm = {
+  name: string;
+};
 const UpdateProfile: React.FC = () => {
-  const [myRecord, setmyRecord] = useState<any>();
-  const [data, setdata] = useState<any>();
-  const [newName, setNewName] = useState("");
+  const { toast } = useToast();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const rec = await dwnReadSelfReturnRecordAndData(selfProfileProtocol);
-      if (rec) {
-        setmyRecord(rec.record);
-        setdata(rec.data);
-      }
-      console.log(
-        "🚀 ~ file: UpdateProfile.tsx:16 ~ fetchData ~ myRecord:",
-        rec,
-      );
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }, //TODO Add this to the form
+  } = useForm<UpdateForm>();
 
-    fetchData();
-    console.log(
-      "🚀 ~ file: UpdateProfile.tsx:21 ~ useEffect ~ fetchData:",
-      fetchData,
-    );
-  }, []);
+  const res = useRecoilValue(dwnReadSelfReturnRecordAndDataSelector);
+  if (!res) return <>Can't find own profile</>;
+  const { record, data, id } = res;
 
-  const updateName = async () => {
-    data.name = newName;
-    const { status } = await myRecord.update({ data });
+  const updateFormSubmit: SubmitHandler<UpdateForm> = async (formData) => {
+    console.log("record", record);
+    console.log("data", data);
+    if (!record) {
+      toast({
+        title: "Error",
+        description: `Couldn't find your dwn record`,
+      });
+      return;
+    }
+    console.log("compose", { ...data, name: formData.name });
+    const { status } = await record.update({
+      data: { name: formData.name },
+    });
+    toast({
+      title: "Posted profile update to DWN",
+      description: `ID: ${id}, Status: ${status.code} (${status.detail})`,
+    });
     console.log(
       "🚀 ~ file: UpdateProfile.tsx:23 ~ updateName ~ status:",
       status,
@@ -42,23 +48,16 @@ const UpdateProfile: React.FC = () => {
 
   return (
     <>
-      {" "}
       {data === undefined ? (
         <h2>Loading... </h2>
       ) : (
-        <>
+        <form>
           <h2>Hello {data.name}</h2>
-          <Input
-            name="nameInput"
-            defaultValue={data.name}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-        </>
-      )}
-      {newName ? (
-        <Button onClick={() => updateName()}>Change Name</Button>
-      ) : (
-        <> </>
+          <Input defaultValue={data.name} {...register("name")} />
+          <Button type={"submit"} onClick={handleSubmit(updateFormSubmit)}>
+            Update
+          </Button>
+        </form>
       )}
     </>
   );
